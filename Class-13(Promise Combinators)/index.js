e2c417f598ac7e31220537cb2fcb162d557ad5fc
mrinal1224 <iammrinalkc@gmail.com>
2026-01-14 15:36:01 +0530

    promise.all

--- Class-13(Promise Combinators)/index.js
+++ Class-13(Promise Combinators)/index.js
@@ -1,24 +1,48 @@
 function fetchUserData() {
-    return new Promise((resolve) => {
-      setTimeout(() => resolve({ userId: 1, username: "JohnDoe" }), 1000);
-    });
+  return new Promise((resolve) => {
+    setTimeout(() => resolve({ userId: 1, username: "JohnDoe" }), 1000);
+  });
+}
+
+function fetchUserPosts(data) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(["Post 1", "Post 2", "Post 3"]), 1000);
+  });
+}
+
+function fetchUserComments() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let success = Math.random() > 0.1; // 50% chance of failure
+      if (success) {
+        resolve(["Nice!", "Interesting post", "Subscribed!"]);
+      } else {
+        reject("Failed to fetch comments ❌");
+      }
+    }, 800);
+  });
+}
+
+// Promise Chaining
+
+//   fetchUserData().then(function(data){
+//     console.log(data)
+//     return fetchUserPosts()
+//   }).then(function(posts){
+//     console.log(posts)
+//     return fetchUserComments()
+//   }).then(function(comments){
+//      console.log(comments)
+//   })
+
+// Promise Combinators
+
+// Promise.all
+
+Promise.all([fetchUserData(), fetchUserPosts(), fetchUserComments()]).then(
+  function (results) {
+    console.log(results[0]);
+    console.log(results[1]);
+    console.log(results[2])
   }
-  
-  function fetchUserPosts(data) {
-    return new Promise((resolve) => {
-      setTimeout(() => resolve(["Post 1", "Post 2", "Post 3"]), 1000);
-    });
-  }
-  
-  function fetchUserComments() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        let success = Math.random() > 0; // 50% chance of failure
-        if (success) {
-          resolve(["Nice!", "Interesting post", "Subscribed!"]);
-        } else {
-          reject("Failed to fetch comments ❌");
-        }
-      }, 800);
-    });
-  }
+);
